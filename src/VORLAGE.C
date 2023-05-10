eb2048d6303746a4e1ca61f1aff83cbad870048d
Dustin <79335703+duha887b@users.noreply.github.com>
2023-05-10 17:01:15 +0200

    Add files via upload

---
+++ src/VORLAGE.C
@@ -0,0 +1,24 @@
+// ###############################
+// Gruppe: 			JAN-1-???
+// Bearbeiter:	Musterfrau, Maxi
+//							Mustermann, Max
+// Datum:				dd.05.2022
+// ###############################
+
+#include <reg167.h>
+#include <stdio.h>
+
+// Variablen
+
+
+// Funktionen
+
+void main(){
+
+	// Ihr Programm
+	
+
+  while(1){
+  
+  }//end while(1)
+}//end main()
