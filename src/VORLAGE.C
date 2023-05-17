e1d4dbbbcd0a42cffc230632ac74ba9ed08752f3
Dustin <dustin.hanusch@mailbox.tu-dresden.de>
2023-05-17 15:23:26 +0200

    update

--- src/VORLAGE.C
+++ src/VORLAGE.C
@@ -7,16 +7,75 @@
 
 #include <reg167.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+sbit signal_1 = P8^0;	//Encoder
+sbit signal_2 = P8^1;
+
+sbit led0 = P2^0;		//LED Leiste
+sbit led1 = P2^1;
+sbit led2 = P2^2;
+sbit led3 = P2^3;
+sbit led4 = P2^4;
+sbit led5 = P2^5;
+sbit led6 = P2^6;
+sbit led7 = P2^7;
+sbit led8 = P2^8;
+sbit led9 = P2^9;
+sbit led10 = P2^10;
+sbit led11 = P2^11;
+sbit led12 = P2^12;
+sbit led13 = P2^13;
+sbit led14 = P2^14;
+sbit led15 = P2^15;
+
+unsigned int activeLEDs = 0;
 
 
-// Variablen
+void led_controle(unsigned int  number,bit  state){
+	switch (number){
+		case 0: led0 = state;
+		case 1: led1 = state;
+		case 2: led2 = state;
+		case 3: led3 = state;
+		case 4: led4 = state;
+		case 5: led5 = state;
+		case 6: led6 = state;
+		case 7: led7 = state;
+		case 8: led8 = state;
+		case 9: led9 = state;
+		case 10: led10 = state;
+		case 11: led11 = state;
+		case 12: led12 = state;
+		case 13: led13 = state;
+		case 15: led14 = state;
+		case 16: led15 = state;
+}
+	}
+
+void LEDBar(unsigned int nLEDs){
+	unsigned int n;
+	
+	for(n=0; n<=nLEDs;n++){
+		led_controle(n,1);
+	}
+	for(n = nLEDs+1; n<=15;n++){
+		led_controle(n,0);
+	}
+		
+}
 
 
-// Funktionen
+void setup(void){
+	activeLEDs = 7;
+	LEDBar(activeLEDs);
+	
+	
+}
 
 void main(){
 
-	// Ihr Programm
+	setup();
 	
 
   while(1){
